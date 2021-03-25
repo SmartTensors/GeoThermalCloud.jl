db2e556118d799d6bf09f03a53788ee53c7cf75d
monty <velimir.vesselinov@gmail.com>
2021-03-25 09:03:50 -0600

    SWNM notebook

---
+++ SWNM/notebook/SWNM.jl
@@ -0,0 +1,139 @@
+import Cairo
+import NMFk
+import DelimitedFiles
+import JLD
+import Gadfly
+import Fontconfig
+import Mads
+
+cd("../");
+
+d, h = DelimitedFiles.readdlm("data/Pepin_PCA_Input_Data_LANL.csv", ','; header=true);
+
+attributes_short = ["Boron"; "Gravity"; "Magnetic"; "Dikes"; "Drainage"; "FaultInter"; "QuatFaults"; "Seismicity"; "NMFaults"; "Springs"; "Vents"; "Lithium"; "Precip"; "Air_Temp"; "Silica"; "Subcrop"; "WT_Gradient"; "WT_Elev"; "Heatflow"; "GS_Elev"; "DTW"; "Crst_Thick"; "Bsmt_Depth"]
+attributes_long = uppercasefirst.(lowercase.(["Boron Concentration"; "Gravity Anomaly"; "Magnetic Intensity"; "Volcanic Dike Density"; "Drainage Density"; "Fault Intersection Density"; "Quaternary Fault Density"; "Seismicity"; "State Map Fault Density"; "Spring Density"; "Volcanic Vent Density"; "Lithium Concentration"; "Precipitation"; "Air Temperature"; "Silica Geothermometer"; "Subcrop Permeability"; "Hydraulic Gradient"; "Watertable Elevation"; "Heat flow"; "Groundsurface Elevation"; "Watertable Depth"; "Crustal Thickness"; "Depth to Basement"]))
+attributes_long_new = uppercasefirst.(lowercase.(["Boron"; "Gravity anomaly"; "Magnetic intensity"; "Volcanic dike density"; "Drainage density"; "Fault intersection density"; "Quaternary fault density"; "Seismicity"; "State map fault density"; "Spring density"; "Volcanic vent density"; "Lithium"; "Precipitation"; "Air temperature"; "Silica geothermometer"; "Subcrop permeability"; "Hydraulic gradient"; "Watertable elevation"; "Heat flow"; "Groundsurface elevation"; "Watertable depth"; "Crustal thickness"; "Depth to basement"]))
+attributes_ordered = ["Boron concentration", "Lithium concentration", "Drainage density", "Spring density", "Hydraulic gradient", "Precipitation", "Gravity anomaly", "Magnetic intensity", "Seismicity", "Silica geothermometer", "Heat flow", "Crustal thickness", "Depth to basement", "Fault intersection density", "Quaternary fault density", "State map fault density", "Volcanic dike density", "Volcanic vent density"];
+
+attributes_remove = uppercasefirst.(lowercase.(["Air Temperature"; "Subcrop Permeability"; "Watertable Elevation"; "Groundsurface Elevation"; "Watertable Depth"]));
+
+index_remove = indexin(attributes_remove, attributes_long)
+attributes_cols = trues(length(attributes_long))
+attributes_cols[index_remove] .= false
+cols = vec(4:26)[attributes_cols]
+attributes = attributes_long[cols .- 3];
+print("Attributes used are:")
+attributes
+
+locations_short = ["Alamos spr";
+"Allen spr";
+"Apache well";
+"Aragon spr";
+"Ash spr";
+"B.Iorio well";
+"Cliff spr";
+"Dent well";
+"Derry spr";
+"Faywood spr";
+"Fed H1 well";
+"Freiborn spr";
+"Garton well";
+"Gila spr 1";
+"Gila spr 2";
+"Goat spr";
+"Jerry well";
+"Kennecott well";
+"Laguna Pbl";
+"Lightning Dock";
+"Los Alturas";
+"Mangas spr";
+"Mimbres spr";
+"Ojitos spr";
+"Ojo Caliente";
+"Ojo Canas";
+"Pueblo well";
+"Radium spr";
+"Rainbow spr";
+"Riverside well";
+"Sacred spr";
+"Socorro Can";
+"Spring";
+"Spring Can";
+"T or C spr";
+"Turkey spr";
+"Victoria well";
+"Warm spr";
+"Well 1";
+"Well 2";
+"Well 3";
+"Well 4";
+"Well 5";
+"Carne well"]
+
+locations_long = ["Alamos Spring";
+"Allen Springs";
+"Apache Tejo Warm Springs well";
+"Aragon Springs";
+"Ash Spring";
+"B. Iorio 1 well";
+"Cliff Warm Spring";
+"Dent windmill well";
+"Derry Warm Springs";
+"Faywood Hot Springs";
+"Federal H 1 well";
+"Freiborn Canyon Spring";
+"Garton well";
+"Gila Hot Springs 1";
+"Gila Hot Springs 2";
+"Goat Camp Spring";
+"Jerry well";
+"Kennecott Warm Springs well";
+"Laguna Pueblo";
+"Lightning Dock";
+"Los Alturas Estates";
+"Mangas Springs";
+"Mimbres Hot Springs";
+"Ojitos Springs";
+"Ojo Caliente";
+"Ojo De las Canas";
+"Pueblo windmill well";
+"Radium Hot Springs";
+"Rainbow Spring";
+"Riverside Store well";
+"Sacred Spring";
+"Socorro Canyon";
+"Spring";
+"Spring Canyon Warm Spring";
+"Truth or Consequences spring";
+"Turkey Creek Spring";
+"Victoria Land and Cattle Co. well";
+"Warm Springs";
+"Well 1";
+"Well 2";
+"Well 3";
+"Well 4";
+"Well 5";
+"Well south of Carne"];
+
+dindex = d[:,end] .== 1
+rows = convert.(Int32, d[dindex,end-1])
+locations = locations_short[rows]
+lat = d[dindex, 2]
+lon = d[dindex, 3];
+
+figuredir = "figures-case01"
+resultdir = "results-case01";
+
+nkrange = 2:10;
+
+X = permutedims(d[dindex, cols])
+Xu, nmin, nmax = NMFk.normalizematrix_row!(X);
+
+W, H, fitquality, robustness, aic = NMFk.execute(Xu, nkrange, 1000; resultdir=resultdir, casefilename="nmfk", load=true)
+W, H, fitquality, robustness, aic = NMFk.load(nkrange, 1000; resultdir=resultdir, casefilename="nmfk");
+
+NMFk.getks(nkrange, robustness[nkrange])
+
+NMFk.plot_signal_selecton(nkrange, fitquality, robustness; figuredir=figuredir, xtitle="Number of signatures")
+
+NMFk.clusterresults(NMFk.getks(nkrange, robustness[nkrange]), W, H, attributes, locations; lat=lat, lon=lon, resultdir=resultdir, figuredir=figuredir, Hcasefilename="locations", Wcasefilename="attributes");
